ebf3466185974c4e8b9e7696902f2044c54fba8f
ZerGo0 <18653821+ZerGo0@users.noreply.github.com>
2024-09-25 19:46:57 +0200

    Added GITHUB_TOKEN env var check and user confirmation

--- release.sh
+++ release.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# make sure that the GITHUB_TOKEN env var is set
+if [ -z "$GITHUB_TOKEN" ]; then
+  echo "GITHUB_TOKEN is not set"
+  exit 1
+fi
+
 # make sure that the repo is clean
 DIRTY=$(git status --porcelain)
 if [ ! -z "$DIRTY" ]; then
@@ -9,13 +15,13 @@ fi
 
 git pull origin master
 
-# get the latest tag
 LATEST_TAG=$(git describe --abbrev=0 --tags)
-
-# increment the minor version
 NEXT_VERSION=$(echo $LATEST_TAG | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{if(length($NF+1)>length($NF))$(NF-1)++; $NF=sprintf("%0*d", length($NF), ($NF+1)%(10^length($NF))); print}')
 
 echo "New version: $NEXT_VERSION"
+
+# ask user for confirmation
+read -p "Are you sure? " -n 1 -r
 
 git tag -a "$NEXT_VERSION" -m "Version $NEXT_VERSION"
 git push origin "$NEXT_VERSION"
